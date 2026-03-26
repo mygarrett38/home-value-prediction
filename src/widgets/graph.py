b448e16518038ae87c8dac072d6320834af6f418
mygarrett38 <mygarrett38@gmail.com>
2026-03-26 14:39:29 -0400

    Make graph ranges dynamic to edges of data

--- src/widgets/graph.py
+++ src/widgets/graph.py
@@ -22,9 +22,9 @@ X_AXIS_NAMES = {
 }
 
 X_AXIS_SCALES = {
-    GraphMode.PREDICTION_RANGE: ((-50, 1050), (0, 0)),
-    GraphMode.ACREAGE_SQ_FT: ((-0.1, 3.1), (-100, 6100)),
-    GraphMode.BED_BATH: ((-0.5, 10.5), (-0.5, 8.5))
+    GraphMode.PREDICTION_RANGE: ((0, 1000), (0, 0)),
+    GraphMode.ACREAGE_SQ_FT: ((0, 3), (0, 6000)),
+    GraphMode.BED_BATH: ((0, 10), (0, 8))
 }
 
 Y_AXIS_NAMES = {
@@ -34,9 +34,9 @@ Y_AXIS_NAMES = {
 }
 
 Y_AXIS_SCALES = {
-    GraphMode.PREDICTION_RANGE: ((-0.5, 10.5), (0, 0)),
-    GraphMode.ACREAGE_SQ_FT: ((-50, 1050), (-50, 1050)),
-    GraphMode.BED_BATH: ((-50, 1050), (-50, 1050))
+    GraphMode.PREDICTION_RANGE: ((0, 10), (0, 0)),
+    GraphMode.ACREAGE_SQ_FT: ((0, 1000), (0, 1000)),
+    GraphMode.BED_BATH: ((0, 1000), (0, 1000))
 }
 
 class GraphDisplay(FigureCanvas):
@@ -52,8 +52,10 @@ class GraphDisplay(FigureCanvas):
         self.refresh(None)
 
     def refresh(self, currentProperty: Property | None):
-        Y_AXIS_SCALES[GraphMode.PREDICTION_RANGE] = ((-0.5, len(self.configuration) - 0.5), (0, 0))
         graphMode = self.configuration.graphMode
+
+        x_lim = list(X_AXIS_SCALES[graphMode])
+        y_lim = list(Y_AXIS_SCALES[graphMode])
 
         def propertyData(prop: Property, graph_num: int) -> tuple[float, float]:
             prop_data = {
@@ -74,11 +76,6 @@ class GraphDisplay(FigureCanvas):
         self.axes_m.set_title(TITLE_NAMES[graphMode][0])
         self.axes_s.set_title(TITLE_NAMES[graphMode][1])
 
-        self.axes_m.set_xlim(*X_AXIS_SCALES[graphMode][0])
-        self.axes_m.set_ylim(*Y_AXIS_SCALES[graphMode][0])
-        self.axes_s.set_xlim(*X_AXIS_SCALES[graphMode][1])
-        self.axes_s.set_ylim(*Y_AXIS_SCALES[graphMode][1])
-
         match graphMode:
             case GraphMode.ACREAGE_SQ_FT | GraphMode.BED_BATH:
                 df_m = pd.DataFrame([
@@ -92,6 +89,13 @@ class GraphDisplay(FigureCanvas):
                     group[graphMode][1] for group in [X_AXIS_NAMES, Y_AXIS_NAMES]
                 ])
 
+                df_mmax = (max(df_m[X_AXIS_NAMES[graphMode][0]].max(), x_lim[0][1]), max(df_m[Y_AXIS_NAMES[graphMode][0]].max(), y_lim[0][1]))
+                df_smax = (max(df_s[X_AXIS_NAMES[graphMode][1]].max(), x_lim[1][1]), max(df_s[Y_AXIS_NAMES[graphMode][1]].max(), y_lim[1][1]))
+                df_moff = np.divide(df_mmax, 20.0)
+                df_soff = np.divide(df_smax, 20.0)
+                x_lim = ((0 - df_moff[0], df_mmax[0] + df_moff[0]), (0 - df_soff[0], df_smax[0] + df_soff[0]))
+                y_lim = ((0 - df_moff[1], df_mmax[1] + df_moff[1]), (0 - df_soff[1], df_smax[1] + df_soff[1]))
+
                 df_m.plot.scatter(x=X_AXIS_NAMES[graphMode][0], y=Y_AXIS_NAMES[graphMode][0], s=100, c="#ee0000", ax=self.axes_m)
                 if currentProperty is not None: self.axes_m.plot(*propertyData(currentProperty, 0), marker="o", ms=15, zorder=2, color="#00ee00")
                 df_s.plot.scatter(x=X_AXIS_NAMES[graphMode][1], y=Y_AXIS_NAMES[graphMode][1], s=100, c="#0000ee", ax=self.axes_s)
@@ -103,6 +107,10 @@ class GraphDisplay(FigureCanvas):
                 ], columns=["Minimum", "Prediction", "Maximum"])
                 df_m.insert(0, "Property", [prop.location.address for prop in self.configuration])
 
+                x_max = max(df_m["Maximum"].max(), 1000)
+                x_lim = ((x_max / -20.0, x_max * 1.05), (0, 0))
+                y_lim = ((-0.5, len(self.configuration) - 0.5), (0, 0))
+
                 df_m.apply(lambda row: self.axes_m.plot((row["Minimum"], row["Maximum"]), (row["Property"],) * 2, color="#888888", linewidth=4), axis=1)
                 df_m.plot.scatter(x="Minimum",    y="Property", s=100, c="#eeaa00", zorder=3, ax=self.axes_m)
                 df_m.plot.scatter(x="Prediction", y="Property", s=100, c="#00ee00", zorder=4, ax=self.axes_m)
@@ -113,5 +121,10 @@ class GraphDisplay(FigureCanvas):
                 
                 self.axes_m.set_xlabel(X_AXIS_NAMES[graphMode][0])
                 self.axes_m.set_ylabel(Y_AXIS_NAMES[graphMode][0])
+
+        self.axes_m.set_xlim(*x_lim[0])
+        self.axes_m.set_ylim(*y_lim[0])
+        self.axes_s.set_xlim(*x_lim[1])
+        self.axes_s.set_ylim(*y_lim[1])
 
         self.figure.canvas.draw()
